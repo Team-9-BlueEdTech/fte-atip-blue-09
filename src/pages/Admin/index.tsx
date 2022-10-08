8dcf95ac0b90bae67f9c9a912279d68e8710142c
Bruno Sallum <brusallum@hotmail.com>
2022-10-07 22:31:52 -0300

    feat(admin-home): functional

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,37 +1,106 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
-import Input from "../../components/Input";
 import Listing from "../../components/Listing/partner";
 import { MockedPartnerList } from "../../mocks/route-admin-partners";
 import { PartnerIndex } from "../../types";
+import { Div } from "../Login/styles";
 import * as S from "./styles";
 
-const Admin = () => {  
+const Admin = () => {
+
+  const navigate = useNavigate();
+
+  const [partnersList, setPartnersList] = useState<PartnerIndex[] | undefined>(undefined);
+  const [search, setSearch] = useState<string>("");
+  const [showFilter, setShowFilter] = useState<boolean>(false);
+
+  console.log(partnersList);
+  
+
+  const loadPartners = () => {
+    //api.get
+    setPartnersList(MockedPartnerList)
+  }
+
+  useEffect(() => {
+    loadPartners()
+  }, [])
+
+  const searchPartner = (partner: PartnerIndex) => {
+    if (search === "") {
+      return partner;
+    } else if (partner.name.toLowerCase().includes(search)) {
+      return partner;
+    }
+  }
+
+  const setFilter = (filter: string) => {
+
+    let sortedList: PartnerIndex[] | undefined;
+
+    if (filter === "A") {
+      sortedList = partnersList?.sort((a, b) => {
+        if (b.name > a.name) return -1;
+        if (b.name < a.name) return 1;
+        return 0;
+      })
+    }
+    if (filter === "Z") {
+      sortedList = partnersList?.sort((a, b) => {
+        if (b.name > a.name) return 1;
+        if (b.name < a.name) return -1;
+        return 0;
+      })
+    }
+
+    setPartnersList(sortedList)
+    setShowFilter(false)
+  }
+
   return (
     <>
       <S.MainDashboard>
         <S.NavDashboard>
-          <Button
-            variant="add"
-            text="Cadastrar"
+          <Button variant="add" text="Cadastrar"
+            onClick={() => navigate("/partner/new")}  
           />
-          <Input
-            inputSize="large"
+          <input type="text"
+            placeholder="🔎  Digite o nome de uma empresa"
+            onChange={(e) => setSearch(e.target.value)}
           />
-          <Button
-            variant="small"
-            text="⬆⬇"
+          <Button text="Filtrar" 
+            onClick={() => setShowFilter(!showFilter)}
           />
         </S.NavDashboard>
         <S.DivDashboard>
-          {MockedPartnerList.map(
-            (partner: PartnerIndex) =>
-              <Listing
-                id={partner.id}
-                name={partner.name}
-                submit={partner.submit}
-                createdAt={partner.createdAt}
-              />
+          {partnersList?.filter(searchPartner).map((partner, index) =>
+            <Listing
+              key={index}
+              id={partner.id}
+              name={partner.name}
+              submit={partner.submit}
+              createdAt={partner.createdAt}
+            />
           )}
+          {
+            showFilter ?
+              <S.FilterDashboard>
+                <Button variant="small" text="A ⬇"
+                 onClick={() => setFilter("A")}
+                />
+                <Button variant="small" text="Z ⬇"
+                  onClick={() => setFilter("Z")}
+                />
+                {/* <Button variant="small" text="⌛ ⬇"
+                  onClick={() => setFilter("New")}
+                />
+                <Button variant="small" text="⌛ ⬆"
+                  onClick={() => setFilter("Old")}
+                /> */}
+              </S.FilterDashboard>
+            : null
+          }
         </S.DivDashboard>
       </S.MainDashboard>
     </>
--- src/pages/Admin/styles.ts
+++ src/pages/Admin/styles.ts
@@ -27,5 +27,27 @@ export const DivDashboard = styled.div`
   background-color: #F3F3F3;
   border-radius: 2rem;
   padding: 2rem;
+  
+  position: relative;
+
+`
+
+export const FilterDashboard = styled.div`
+
+  width: 20rem;
+  height: 5rem;
+  background-color: white;
+  border: solid 2px;
+  border-radius: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+
+
 
 `
