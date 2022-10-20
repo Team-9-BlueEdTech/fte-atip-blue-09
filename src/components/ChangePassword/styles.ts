62b8d39da122f1f73e5e596dd729ad45a0316ede
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-10-20 20:02:38 -0300

    feat(ChangePassword): component styled

--- src/components/ChangePassword/styles.ts
+++ src/components/ChangePassword/styles.ts
@@ -12,3 +12,20 @@ export const MainDiv = styled.main`
   align-items: center;
 
 `
+export const DivInput = styled.div`
+
+width: 100%;
+height: 40vh;
+align-items: center;
+display: flex;
+flex-direction: column;
+margin: 20px;
+border-radius: 1rem;
+
+input {
+  margin: 20px;
+}
+
+
+`
+
