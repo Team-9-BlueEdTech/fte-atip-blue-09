10dd6e3090a204a6fa77a93075d3931499615493
Bruno Sallum <brusallum@hotmail.com>
2022-10-23 18:34:17 -0300

    feat(data distributing)

--- src/components/CensusChartCard/index.tsx
+++ src/components/CensusChartCard/index.tsx
@@ -1,14 +1,12 @@
 import * as S from './styles'
 
-const PieChartCard = ({ title }) => {
-
-
- return (
+const ChartCard = ({ title, data }) => {
+  return (
     <S.DivCard>
       <h3>{title}</h3>
-
+      {data}
     </S.DivCard>   
   )
 }
 
-export default PieChartCard
+export default ChartCard
--- src/components/CensusDashboard/index.tsx
+++ src/components/CensusDashboard/index.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useCensus } from "../../contexts/census"
-import PieChartCard from "../PieChartCard";
+import { useCensus } from "../../contexts/census";
+import { Answers } from "../../types";
+import { MockedAnswers } from "../../mocks/route-answers-by-census-id";
+import ChartCard from "../CensusChartCard";
 import * as S from './styles'
 
 const CensusDashboard = () => {
 
   const { censusId } = useParams();
   const { census, getCensusById } = useCensus();
+  const [ answers, setAnswers ] = useState<Answers[]>();
 
   const mainCharts: string[] = [ // this array can be added to the DB
     "Raça", "Identidade de Gênero",
@@ -17,15 +20,19 @@ const CensusDashboard = () => {
   useEffect(() => {
     if (censusId)
       getCensusById(censusId)
-  }, [])  
+      setAnswers(MockedAnswers)
+  }, [])
 
- return (
+  return (
     <S.DivDashboard>
       {
-        mainCharts.map((chartName, index) => {
-          return <PieChartCard
+        census && answers && mainCharts.map((chartName, index) => {
+          return <ChartCard
             key={index}
             title={chartName}
+            data={answers.map(answer => {
+              return answer.list[census.questionsLabels.indexOf(chartName)]
+            })}
           />
         })
       }
